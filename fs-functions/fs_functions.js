89037e9f33797d99845bd0aa3aae2fac08ea54e3
Nawid Arefi <130712567+NawidTechTamer@users.noreply.github.com>
2025-07-08 21:39:08 +0430

    fs functions and all read, write, delete and update functions

---
+++ fs-functions/fs_functions.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+
+// Make a directory
+
+// const my_folder = fs.mkdir("./txt-folder", (err) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log("Folder created successfully");
+// });
+
+// Write text file in txt-folder
+
+// const my_file = fs.writeFile(
+//   "./txt-folder/my_file.txt",
+//   "Hello, this is a test file.",
+//   (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log("Your text file successfully created in txt-folder");
+//   }
+// );
+
+// Append in file
+
+// const append_file = fs.appendFile(
+//   "./txt-folder/my_file.txt",
+//   "\nThis is a appended text",
+//   (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log("your file successfully appended in txt-folder");
+//   }
+// );
+
+// Read file from txt-folder
+
+// const read_file = fs.readFile("./txt-folder/my_file.txt", (err, data) => {
+//   if (err) console.log(err);
+//   console.log("Your data: ", data.toString());
+// });
+
+// Remove file from txt-folder
+
+// const remove_file = fs.unlink("./txt-folder/my_file.txt", (err) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log("Your file successfully removed from txt-folder");
+// });
+
+// Remove the directory
+
+// const remove_dir = fs.rmdir("./txt-folder", (err) => {
+//   if (err) console.log(err);
+//   console.log("Your dirctory successfully removed.");
+// });
